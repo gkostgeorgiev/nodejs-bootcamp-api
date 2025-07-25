8d27ec94be0ebf248a62a0af593e051abdc190e3
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2025-07-25 17:49:48 +0300

    installed colors to highlight certain kinds of messages in the terminal

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB connected successfully");
+  console.log("MongoDB connected successfully".green.underline.bold);
 };
 
 module.exports = connectDB;
