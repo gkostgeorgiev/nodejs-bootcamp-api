c2110e9c0497f4c32aed203395fd99824db1dd1b
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2025-07-25 14:48:30 +0300

    Installed morgan - logging middleware for development purposes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
@@ -10,11 +10,13 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
-
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
