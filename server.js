ed001ff40f890d87da7d32105f15efcdf20633c5
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2025-07-25 13:51:03 +0300

    Created and implemented main types of routes.

--- server.js
+++ server.js
@@ -8,6 +8,41 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: "Show all bootcamps",
+  });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Get bootcamp with ID ${req.params.id}`,
+  });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(201).json({
+    success: true,
+    msg: "Create new bootcamp",
+  });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp with ID ${req.params.id}`,
+  });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp with ID ${req.params.id}`,
+  });
+});
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`)
