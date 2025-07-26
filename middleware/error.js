f672884f48284be9763f1c2dea07b4b713d34960
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2025-07-26 19:31:15 +0300

    Added custom error messages and status code in error middleware for duplication and validation errors.

--- middleware/error.js
+++ middleware/error.js
@@ -13,6 +13,20 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || "Server Error",
