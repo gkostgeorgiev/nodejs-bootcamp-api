be75814374e9cd249b39bdbd318ee4dd5c3dec63
Georgi Kostadinov Georgiev <georgi.ko.georgiev@a1.bg>
2025-08-21 13:39:42 +0300

    Added cookie parser. Created utility function to send token response including a cookie w/ the expiration date

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create Token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login user
@@ -29,7 +26,9 @@ exports.login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new ErrorResponse("Please provide an email and a password", 400));
+    return next(
+      new ErrorResponse("Please provide an email and a password", 400)
+    );
   }
 
   // Check for user
@@ -46,8 +45,26 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
 
-  // Create Token
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true; // Serve cookies over HTTPS in production
+  }
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
