38ffa9bf71fe0c31e5aac78e3a27386ee8fbf7dc
Georgi Kostadinov Georgiev <georgi.ko.georgiev@a1.bg>
2025-08-22 10:51:08 +0300

    Implemented update user details and update user password request handlers.

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const { register, login, getMe, forgotPassword, resetPassword } = require("../controllers/auth");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+} = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
 const router = express.Router();
@@ -9,5 +17,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 
 module.exports = router;
