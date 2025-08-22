366a378b1b237dc28b9eb648128fa082d22c6c54
Georgi Kostadinov Georgiev <georgi.ko.georgiev@a1.bg>
2025-08-22 15:39:05 +0300

    Implemented edit and delete reviews by their creators or an admin. Completed full CRUD functionality for reviews.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -67,3 +67,73 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @desc    Edit review
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review)
+    return next(
+      new ErrorResponse(`No review found with id ${req.params.id}`, 404)
+    );
+
+  // Make sure review belongs to the user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to edit this review`,
+        401
+      )
+    );
+  }
+
+  const updatedReview = await Review.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      text: req.body.text,
+      rating: req.body.rating,
+      user: req.user.id,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  return res.status(200).json({
+    success: true,
+    data: updatedReview,
+  });
+});
+
+// @desc    Delete review
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review)
+    return next(
+      new ErrorResponse(`No review found with id ${req.params.id}`, 404)
+    );
+
+  // Make sure review belongs to the user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to edit this review`,
+        401
+      )
+    );
+  }
+
+  await Review.deleteOne();
+
+  return res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
