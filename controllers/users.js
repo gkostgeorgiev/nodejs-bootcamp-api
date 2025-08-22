16659f1ca6005dca83856625645782ad4e5459de
Georgi Kostadinov Georgiev <georgi.ko.georgiev@a1.bg>
2025-08-22 11:46:06 +0300

    Added user admin controller, router and implemented them in server.js

---
+++ controllers/users.js
@@ -0,0 +1,92 @@
+const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const { sendTokenResponse } = require("./auth");
+
+// @desc    Get all users
+// @route   GET /api/v1/auth/users
+// @access  Private
+exports.getAllUsers = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
+});
+
+// @desc    Get a single user
+// @route   GET /api/v1/auth/users/:id
+// @access  Private
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new ErrorResponse("User not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Create user
+// @route   POST /api/v1/auth/users
+// @access  Private
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Update user
+// @route   PUT /api/v1/auth/users/:id
+// @access  Private
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      email,
+      password,
+      role,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!user) {
+    return next(new ErrorResponse("User not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Delete user
+// @route   DELETE /api/v1/auth/users/:id
+// @access  Private
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return next(new ErrorResponse("User not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
+const User = require("../models/User");
+
+const { protect, authorize } = require("../middleware/auth");
+const advancedResults = require("../middleware/advancedResults");
+
+const router = express.Router({ mergeParams: true });
+
+router.use(protect);
+router.use(authorize("admin"));
+
+router.route("/").get(advancedResults(User), getAllUsers).post(createUser);
+
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
